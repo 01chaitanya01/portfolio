fb53f6243669a40b3a061604cd91d7a17bbc5253
01chaitanya01 <choudharichaitu0101@gmail.com>
2024-02-01 20:03:17 +0530

    changes in the path of every file in app.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Webpage from './component/Webpage';
+// import Webpage from './component/Webpage';
 
-// import HomePage from './component/HomePage'
-// import Navbar from './component/Navbar'
-// import About from './component/About'
-// import Tools from './component/Tools'
-// import Project from './component/Project'
-// import Preloader from './component/Preloader'
-// import Contact from './component/Contact'
-// import Footer from './component/Footer'
+import HomePage from './component/HomePage'
+import Navbar from './component/Navbar'
+import About from './component/About'
+import Tools from './component/Tools'
+import Project from './component/Project'
+import Preloader from './component/Preloader'
+import Contact from './component/Contact'
+import Footer from './component/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        {/* <Router>
             <Routes>
             <Route path='/' element={<Webpage />} />
           </Routes>
-        </Router>
+        </Router> */}
 
-        {/* <Preloader />
+        <Preloader />
         <Navbar />
         <HomePage />
         <Tools />
         <About />
         <Project />
         <Contact />
-        <Footer /> */}
+        <Footer />
       </div>
     );
   }
